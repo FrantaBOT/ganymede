ff4379077a2b2c54534ab60c01c4dea82c9c70f3
FrantaBOT <frantabot@voic.eu>
2025-11-23 01:23:48 +0100

    Reverted few files

--- internal/exec/util.go
+++ internal/exec/util.go
@@ -2,16 +2,28 @@ package exec
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"net/http"
 	"net/url"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/grafov/m3u8"
 	"github.com/rs/zerolog/log"
 	"github.com/zibbp/ganymede/internal/utils"
 )
+
+func killYtDlp(pid int) error {
+	pgid, err := syscall.Getpgid(pid)
+	if err != nil {
+		log.Error().Err(err).Msgf("Failed to get process group ID for PID %d", pid)
+		return fmt.Errorf("failed to get process group ID for PID %d: %w", pid, err)
+	}
+	log.Debug().Msgf("Process group ID for PID %d is %d", pid, pgid)
+	return syscall.Kill(-pgid, syscall.SIGINT)
+}
 
 func tryProxyServer(proxyURL string, testURL string, header string, proxyType utils.ProxyType) (*m3u8.MasterPlaylist, bool) {
 	switch proxyType {
